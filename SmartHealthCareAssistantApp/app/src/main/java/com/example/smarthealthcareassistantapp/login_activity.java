7068221e48cf83715bc7576a72c47b83e3785697
md-rakib-78 <mdrakib53661@gmail.com>
2026-04-09 15:14:22 +0600

    update user authentocation

--- SmartHealthCareAssistantApp/app/src/main/java/com/example/smarthealthcareassistantapp/login_activity.java
+++ SmartHealthCareAssistantApp/app/src/main/java/com/example/smarthealthcareassistantapp/login_activity.java
@@ -2,14 +2,27 @@ package com.example.smarthealthcareassistantapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class login_activity extends AppCompatActivity {
 
-    Button login,signUp;
+    private Button login,signUp;
+    private EditText email,password;
+    FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +37,58 @@ public class login_activity extends AppCompatActivity {
         });
 
         login = findViewById(R.id.signIn);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.pass);
+        progressBar = findViewById(R.id.prog);
+
+
+        mAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(v -> {
-            startActivity(new Intent(login_activity.this, home_dashboard_activity.class));
+
+            String Email = email.getText().toString().trim();
+            String Password = password.getText().toString().trim();
+
+            if(Email.isEmpty()){
+                email.setError("Email is required");
+                email.requestFocus();
+                return;
+            }
+
+            if(!android.util.Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                email.setError("Enter valid email");
+                email.requestFocus();
+                return;
+            }
+
+            if(Password.isEmpty()) {
+                password.setError("Password is required");
+                password.requestFocus();
+                return;
+            }
+
+            if(Password.length() < 6){
+                password.setError("Password must be at least 6 characters");
+                password.requestFocus();
+                return;
+            }
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            mAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    progressBar.setVisibility(View.GONE);
+
+                        startActivity(new Intent(login_activity.this, home_dashboard_activity.class));
+                        finish();
+                }else
+                {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(login_activity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+
         });
 
 
--- SmartHealthCareAssistantApp/app/src/main/java/com/example/smarthealthcareassistantapp/sign_up_Activity.java
+++ SmartHealthCareAssistantApp/app/src/main/java/com/example/smarthealthcareassistantapp/sign_up_Activity.java
@@ -1,6 +1,10 @@
 package com.example.smarthealthcareassistantapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,99 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
 public class sign_up_Activity extends AppCompatActivity {
+
+    private EditText fullName,email,password,Cpassword;
+
+    private Button signUp;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
+
+        fullName = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.pass);
+        Cpassword = findViewById(R.id.Cpass);
+
+        signUp = findViewById(R.id.signup);
+
+        auth = FirebaseAuth.getInstance();
+
+        signUp.setOnClickListener(v -> {
+
+            String fname = fullName.getText().toString();
+            String mail = email.getText().toString();
+            String pass = password.getText().toString();
+            String Cpass = Cpassword.getText().toString();
+
+            if (fname.isEmpty()) {
+                fullName.setError("Full name is required");
+                fullName.requestFocus();
+                return;
+            }
+            if (mail.isEmpty()) {
+                email.setError("Email is required");
+                email.requestFocus();
+                return;
+            }
+
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+                email.setError("Enter valid email");
+                email.requestFocus();
+                return;
+            }
+
+
+            if (pass.isEmpty()) {
+                password.setError("Password is required");
+                password.requestFocus();
+                return;
+            }
+
+            if (pass.length() < 6) {
+                password.setError("Password must be at least 6 characters");
+                password.requestFocus();
+                return;
+            }
+
+            if (!pass.equals(Cpass)) {
+                Cpassword.setError("Password does not match");
+                Cpassword.requestFocus();
+                return;
+            }
+
+            auth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(sign_up_Activity.this, "Registration Successful", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(this, login_activity.class));
+                    finish();
+
+                }else{
+                    Toast.makeText(sign_up_Activity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException)
+                    {
+                        Toast.makeText(sign_up_Activity.this, "User already exists", Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(sign_up_Activity.this, "Error: "+task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+
+            });
+
+        });
+
+
 
     }
 }
